b95b4bfd3c967748610dd62361085f18de29773e
Felipe Acosta Coronado <106710485+Felipe10812@users.noreply.github.com>
2023-05-24 16:35:47 -0600

    Add files via upload

---
+++ Estados de mexico/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex <> -1 Then
+            Label3.Text = ComboBox1.SelectedItem.ToString()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedIndex <> -1 Then
+            Dim estadoSeleccionado As String = ComboBox1.SelectedItem.ToString()
+            Label3.Text = estadoSeleccionado
+
+            MessageBox.Show("Estado seleccionado: " & estadoSeleccionado, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
+
+
